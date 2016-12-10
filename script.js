e2b124a5a5f01d39d378e012fcca2945e2e05da9
Jaymes Phimmachack <phimmachack.jaymes.gmail.com>
2016-12-10 13:21:21 -0500

    Merge code from Lucas repo and update drag and drop

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
+
+
+
+// Drap and Drop
+
+
+function dragstart_handler(event) {
+  console.log(event);
+  event.dataTransfer.setData("text/plain", event.target.id);
+}
+
+function drop_handler(event) {
+  event.preventDefault();
+  var data = event.dataTransfer.getData("text/plain");
+  event.target.appendChild(document.getElementById(data));
+}
+
+function dragover_handler(event) {
+  event.preventDefault();
+  event.dataTransfer.dropEffect = "move";
+}
+
+// Scramble
+
+function click_handler(event) {
+  var bg = document.querySelector('#droppableZone');
+  var pieces = [].slice.call(document.querySelectorAll('.piece'));
+
+  bg.style.backgroundImage = "url('images/_Puzzle_bg_unsolved.png')";
+
+  pieces.forEach(piece => {
+    piece.style.display = 'block'
+    piece.style.position = 'absolute';
+    piece.style.top = Math.floor(Math.random() * (48+1)) + '%';
+    piece.style.left = Math.floor(Math.random() * (87+1)) + '%';
+    document.querySelector('#pieces-tray').appendChild(piece);
+  });
+}
+
+
+
+
+
+// Congradulate User
+
 (function() {
   var animations = ["animate", "animate_rotate", "animate_horizontal", "animate_vertical", "animate_crazy"];
   var elementsToShake = document.querySelectorAll('.piece');
